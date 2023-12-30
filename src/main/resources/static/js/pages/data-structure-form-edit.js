a1d331a0e00929f6509919fcb3a0121fc99b345c
Andy <tambe.ani@northeastern.edu>
2023-12-30 18:10:08 -0500

    fix: Moved quill config to .js file

--- src/main/resources/static/js/pages/data-structure-form-edit.js
+++ src/main/resources/static/js/pages/data-structure-form-edit.js
@@ -4,25 +4,32 @@ import { showToast } from "../common/toast.js";
 
 // Check if the current URL contains "/signup"
 if (window.location.pathname.includes('/data-structures/') && window.location.pathname.includes('/edit')) {
-  // Use DOMContentLoaded directly in signup.js
   document.addEventListener('DOMContentLoaded', () => {
+	
 
+	// Configure Quill Editor
+	const quill = configureQuillEditor();
+	
     // Attach the onSubmit function to the button click event
     const form = document.getElementById('data-structure-form-edit');
     if (form) {
-      form.addEventListener('submit', onUpdate);
+      form.addEventListener('submit', () => onUpdate(event, quill));
     }
-
+    
   });
 }
 
-const onUpdate = (event) => {
+const onUpdate = (event, quill) => {
   // Prevent the default form submission
   event.preventDefault();
+  
+  // Setup Quill Editor text to Form's input
+  document.getElementById('hidden-notes').value = quill.root.innerHTML;
 
   // Fetch Form-data
   const formData = new FormData(event.target);
   const structure = Object.fromEntries(formData.entries());
+  
 
   // Invoke makeRequest(url, method, formData, onSuccess, onError)
   makeRequest('/api/data-structures/' + structure.id, 'PATCH', structure, onSuccess, onError);
@@ -37,5 +44,21 @@ const onSuccess = (data) => {
 const onError = (error) => {
   showToast("Request Failed", "fail");
 };
+
+const configureQuillEditor = () => {
+    const quill = new Quill('#quill-editor', {
+            theme: 'snow',
+            modules: {
+                toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'],
+                    [{ 'code-block': true }], // This adds a button for code block
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                    ['image'],
+                ],
+            },
+        });
+        
+	return quill;
+}
 
 export { onUpdate };
